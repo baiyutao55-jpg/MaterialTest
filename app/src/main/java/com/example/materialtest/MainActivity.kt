c55157c0718a4942467ad3ace99520c22acdbfe4
byt <282426833@qq.com>
2026-01-29 23:25:35 +0800

    MaterialTest commit

--- app/src/main/java/com/example/materialtest/MainActivity.kt
+++ app/src/main/java/com/example/materialtest/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.materialtest
 
 import android.os.Bundle
 import android.util.Log
-import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,11 +16,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
        val toolbar=findViewById<Toolbar>(R.id.toolbar)
-      // setSupportActionBar(toolbar)
+        val drawerLayout=findViewById<DrawerLayout>(R.id.drawerlayout)
+        toolbar.setNavigationIcon(R.drawable.ic_menu)
+
+        toolbar.setNavigationOnClickListener { drawerLayout.openDrawer(GravityCompat.START) }
+
+        //setSupportActionBar(toolbar)
+
+
+
+        /*
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setDisplayShowHomeEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+
+
+        }*/
+
+
         toolbar?.inflateMenu(R.menu.toolbar)
        toolbar?.setOnMenuItemClickListener {
            item ->
            when (item.itemId) {
+               android.R.id.home-> drawerLayout.openDrawer(GravityCompat.START)
                R.id.backup -> Toast.makeText(
                    toolbar.context,
                    "You clicked Backup",
