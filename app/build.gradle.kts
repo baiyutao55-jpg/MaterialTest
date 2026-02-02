02988d7073ec5a122ce2da21decb261598e9e5d6
byt <282426833@qq.com>
2026-02-02 13:55:44 +0800

    MaterialTest commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -31,6 +32,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    buildFeatures {
+        compose = true
+    }
 }
 
 dependencies {
@@ -47,8 +51,19 @@ dependencies {
 
     implementation(platform("androidx.swiperefreshlayout:swiperefreshlayout:1.0.0"))
     implementation("androidx.swiperefreshlayout:swiperefreshlayout")
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
 
 }
--- app/src/main/java/com/example/materialtest/FruitAdapter.kt
+++ app/src/main/java/com/example/materialtest/FruitAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.materialtest
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -19,8 +20,19 @@ class FruitAdapter(val context: Context,val fruitList:List<Fruit>):
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view= LayoutInflater.from(context).inflate(R.layout.fruit_item,parent,false)
+        val holder=ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position=holder.adapterPosition
+            val fruit=fruitList[position]
+            val intent= Intent(context, FruitActivity::class.java).apply {
+                putExtra(FruitActivity.FRUIT_NAME,fruit.name)
+                putExtra(FruitActivity.FRUIT_IMAGE_ID,fruit.imageid)
+            }
+            context.startActivity(intent)
+        }
 
-        return ViewHolder(view)
+
+        return holder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
